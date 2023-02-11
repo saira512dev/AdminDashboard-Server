6e51da16c838c1070d368f681a605a962eb34e63
saira512dev <512saira@gmail.com>
2023-02-11 10:00:00 -0600

    data inserted into "overallStat" table

--- index.js
+++ index.js
@@ -16,8 +16,9 @@ import User from "./models/User.js"
 import Product from "./models/Product.js"
 import ProductStat from "./models/ProductStat.js"
 import Transaction from "./models/Transaction.js"
+import OverallStat from "./models/OverallStat.js"
 
-import { dataUser, dataProduct, dataProductStat, dataTransaction} from "./data/index.js"
+import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat} from "./data/index.js"
 
 // CONFIGURATION
 dotenv.config({ path: "./config/.env" });
@@ -42,7 +43,7 @@ app.listen(process.env.PORT, () => {
     console.log(`Server is running at port ${process.env.PORT}, you better catch it!`);
 
     //Only add data once
-
+    // OverallStat.insertMany(dataOverallStat)
     // User.insertMany(dataUser)
     // Product.insertMany(dataProduct)
     // ProductStat.insertMany(dataProductStat)
