79ab8dfdf9db9a0a6c748da5e4431a5976cb0cb6
saira512dev <512saira@gmail.com>
2023-02-12 10:00:00 -0600

    Route for accessing data for map added

--- routes/client.js
+++ routes/client.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { getProducts, getCustomers, getTransactions } from "../controllers/client.js"
+import { getProducts, getCustomers, getTransactions, getGeography } from "../controllers/client.js"
 
 const router = express.Router();
 
 router.get("/products", getProducts);
 router.get("/customers", getCustomers);
 router.get("/transactions", getTransactions);
+router.get("/geography", getGeography);
 
 
 export default router;
