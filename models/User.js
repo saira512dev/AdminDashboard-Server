e0327683cd2c8a57bff379746eaa7013baecdfd1
saira512dev <512saira@gmail.com>
2023-02-16 10:00:00 -0600

    "pre" hook added to hash password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -26,10 +27,38 @@ const UserSchema = new mongoose.Schema({
         type: String,
         enum: ["user", "admin", "superadmin"],
         default: "admin"
-    }
+    },
+    password: String
 },
 { timestamps: true}
 )
+
+// Password hash middleware.
+ 
+UserSchema.pre('save', function save(next) {
+    const user = this
+    console.log(this)
+    // if (!user.isModified('password')) { return next() }
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) { return next(err) }
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) { return next(err) }
+        user.password = hash
+        console.log(hash)
+        next()
+      })
+    })
+  })
+  
+  
+  // Helper method for validating user's password.
+  
+  UserSchema.methods.comparePassword = function comparePassword(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+      cb(err, isMatch)
+    })
+  }
+
 
 const User = mongoose.model("User", UserSchema);
 export default User;
