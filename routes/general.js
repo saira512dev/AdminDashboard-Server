d7860fa66a8ac445676db3613cb394472f8bba08
saira512dev <512saira@gmail.com>
2023-03-07 10:00:00 -0600

    Routes for authentication added

--- routes/general.js
+++ routes/general.js
@@ -1,8 +1,13 @@
 import express from "express";
+import  { logout, postLogin, test }  from "../controllers/auth.js";
+import middleWare  from "../middleware/auth.js";
 import { getUser, getDashboardStats } from "../controllers/general.js";
 
 const router = express.Router();
 
+router.post("/login", postLogin);
+router.get("/test", test);
+router.get("/logout", logout);
 router.get("/user/:id", getUser)
 router.get("/dashboard", getDashboardStats)
 
