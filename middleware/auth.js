426eec63db6923f3a65b7bfa12ff7534fb4b5898
saira512dev <512saira@gmail.com>
2023-03-10 12:58:37 +0400

    Fixed auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,9 @@
-export const auth  = {
-    ensureAuth: function (req, res, next) {
+export default function (req, res, next) {
       if (req.isAuthenticated()) {
         return next()
       } else {
         res.redirect('/')
+        console.log("🚀 ~ file: auth.js:8 ~req.user:",req.session)
       }
     }
-  }
   
